2b5ab094fcbac6baec302ad0857a0e839dd592be
pangq2 <pangq@oregonstate.edu>
2018-05-18 14:47:31 -0700

    changes

---
+++ functions/charge.py
@@ -0,0 +1,14 @@
+from sys import *
+import os
+
+def read_ACF():
+    """ read the ACF.dat file
+    parameters
+    ---------------
+    The index number of the atoms
+
+    Return
+    ----------------
+    The charge of the selected atoms
+    """
+
